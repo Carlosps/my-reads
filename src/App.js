87b5a32cc3d74db2a8041e53b51cf1f483c0b6c8
Antonio Reis <carlosreisps@gmail.com>
2019-03-07 00:54:11 -0300

    Added feature of changing book category

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+
 import './App.css';
-import Search from './Search';
+
+// Components
 import ListBook from './ListBooks';
+import Search from './Search';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      if (books[0]) {
+        this.setState({ books });
+      }
+    });
+  }
 
   render() {
     return (
       <div className="app">
         <Route path="/search" render={({ history }) => <Search />} />
-        <Route exact path="/" render={(history) => <ListBook />} />
+        <Route exact path="/" render={history => <ListBook />} />
       </div>
     );
   }
